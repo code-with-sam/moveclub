a3dc37ad95b737b8caad6131dd5477bd88ab7f3a
Sam Billingham <sambillie.sb@gmail.com>
2018-05-23 10:35:52 +0100

    fix - steem comment undefined var as duration

--- routes/post.js
+++ routes/post.js
@@ -34,13 +34,13 @@ router.post('/comment',  util.isAuthenticatedJSON, (req, res) => {
     let author = req.session.steemconnect.name
     let permlink = 'move-club-' + util.urlString()
     let title = POST_TITLE
-    let body = `${POST_TITLE} - @${author} - ${req.body.workoutType} - ${req.body.distance}${req.body.distanceUnit} - ${req.body.workoutDuration}mins`
     let parentAuthor = PARENT
     let parentPermlink = PERMLINK
-    let s = req.body.workoutDurationSeconds !== '' ? req.body.workoutDurationSeconds  : 0
-    let m = req.body.workoutDurationMinutes !== '' ? req.body.workoutDurationMinutes  : 0
-    let h = req.body.workoutDurationHours !== '' ? req.body.workoutDurationHours  : 0
+    let s = req.body.workoutDurationSeconds !== '' ? req.body.workoutDurationSeconds  : '00'
+    let m = req.body.workoutDurationMinutes !== '' ? req.body.workoutDurationMinutes  : '00'
+    let h = req.body.workoutDurationHours !== '' ? req.body.workoutDurationHours  : '00'
     let workoutDurationInSeconds = parseInt(s) + (parseInt(m) * 60) + (parseInt(h) * 60 * 60 )
+    let body = `${POST_TITLE} - @${author} - ${req.body.workoutType} - ${req.body.distance}${req.body.distanceUnit} - ${h}:${m}:${s}`
     let customJson = {
       app: 'move.app/v0.1.0',
       workoutType: req.body.workoutType,
